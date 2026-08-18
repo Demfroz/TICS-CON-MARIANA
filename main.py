87e66634c3a4ae439d5f14ddf225f01c75db97c7
Demfroz <ellolero64@gmail.com>
2026-08-17 20:02:09 -0500

    feat:TERCER CUM

--- main.py
+++ main.py
@@ -3,11 +3,6 @@
 from src.planilla import cargar_trabajadores, generar_planilla, mostrar_planilla
 
 
-def main():
-    trabajadores = cargar_trabajadores()
-    planilla = generar_planilla(trabajadores)
-    mostrar_planilla(planilla)
-
 
 if __name__ == "__main__":
     main()
