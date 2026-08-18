35461b51f7ee02c695c7c0e92b29f0d8b1007f6a
Demfroz <ellolero64@gmail.com>
2026-08-17 20:03:09 -0500

    feat: 4 INTENTO

--- main.py
+++ main.py
@@ -2,6 +2,11 @@
 
 from src.planilla import cargar_trabajadores, generar_planilla, mostrar_planilla
 
+def main():
+    trabajadores = cargar_trabajadores()
+    planilla = generar_planilla(trabajadores)
+    mostrar_planilla(planilla)
+
 
 
 if __name__ == "__main__":
